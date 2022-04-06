c571d1b5df10a8a1c20846c4a0ac285ab4404bc8
ethanp1 <ethanp1@co2061-13.ece.iastate.edu>
2022-04-05 19:22:42 -0500

    structs

--- appserver.h
+++ appserver.h
@@ -2,12 +2,17 @@ typedef struct account{ // used to hold the mutex of each account and balance
 	pthread_mutex_t mutex;
 	int balance;
 } account; 
+typedef struct trans{
+	int acc_id;
+	int amount;
+}trans
 
 typedef struct operation{
 	struct operation* nextop;
 	int ID;
 	int check_op;
-	int trans_op;
+	struct trans* transcations;
+	int num_trans;
 	struct timeval starttime;
 }operation;
 
